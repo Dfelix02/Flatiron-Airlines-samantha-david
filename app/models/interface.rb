5d44471362f5813d09b70d3dbd16a19179a84fe2
Dfelix02 <david.felix02@hotmail.com>
2020-08-17 16:45:05 -0400

    interface.rb methods: initialize,welcome,user_register_helper,main_menu. These methods are empty atm

--- app/models/interface.rb
+++ app/models/interface.rb
@@ -1,5 +1,26 @@
 class Interface
+    attr_reader :prompt
+    attr_accessor :user
+
     def initialize
         @prompt = TTY::Prompt.new
+    end
+
+    def welcome
+
+    end
+
+    def user_register_helper
+        
+
+
+
+
+        self.main_menu
+    end
+
+    def main_menu
+        user.reload 
+        system "clear" 
     end
 end
