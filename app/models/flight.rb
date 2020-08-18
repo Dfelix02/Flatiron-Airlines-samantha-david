95e03b276224d95c9c63ec14a3fd08fd14fa3506
sbalgobin94 <sbalgobin94@gmail.com>
2020-08-18 18:54:25 -0400

    working on book a flight

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,7 +2,14 @@ class Flight < ActiveRecord::Base
   has_many :reservations
   has_many :users, through: :reservations
 
-  def self.find_flights
+  def self.all_destinations
+    all_destinations = Destination.all.map do |destination|
+      "#{destination.city}, #{destination.country}, #{destination.airport}"
+    end
+  end
+
+  def self.find_flights(date, destination_id)
+    Flight.where(date: date, destination_id: destination_id)
   end
   
 end
