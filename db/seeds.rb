51176d2906b56cb400b0def67aba9d91274ff7ea
sbalgobin94 <sbalgobin94@gmail.com>
2020-08-20 17:38:35 -0400

    edited seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,13 +26,13 @@ sydney = Destination.create(city: "Sydney", country: "Australia", airport: "SYD 
 
 #Flights
 
-to_dubai = Flight.create(date: "2020-08-20", destination_id: dubai.id, departing_time: "2:20 PM", arrival_time: "11:37 PM")
-to_dubai2 = Flight.create(date: "2020-08-20", destination_id: dubai.id, departing_time: "4:22 AM", arrival_time: "9:46 PM")
-to_hong_kong = Flight.create(date: "2020-09-05", destination_id: hong_kong.id, departing_time: "8:00 AM", arrival_time: "10:20 PM")
-to_bangkok = Flight.create(date: "2020-10-05", destination_id: bangkok.id, departing_time: "3:15 PM", arrival_time: "08:17 AM")
-to_rome = Flight.create(date: "2020-11-25", destination_id: rome.id, departing_time: "6:30 AM", arrival_time: "7:27 PM")
-to_tokyo = Flight.create(date: "2021-01-13", destination_id: tokyo.id, departing_time: "7:05 AM", arrival_time: "2:26 AM")
-to_sydney = Flight.create(date: "2020-12-25", destination_id: sydney.id, departing_time: "12:30 PM", arrival_time: "9:45 PM")
+to_dubai = Flight.create(date: "2020-08-21", destination_id: dubai.id, departing_time: "2:20 PM", arrival_time: "11:37 PM", price: 450.07)
+to_dubai2 = Flight.create(date: "2020-08-21", destination_id: dubai.id, departing_time: "4:22 AM", arrival_time: "9:46 PM", price: 522.01)
+to_hong_kong = Flight.create(date: "2020-08-21", destination_id: hong_kong.id, departing_time: "8:00 AM", arrival_time: "10:20 PM", price: 257.02)
+to_bangkok = Flight.create(date: "2020-08-21", destination_id: bangkok.id, departing_time: "3:15 PM", arrival_time: "08:17 AM", price: 301.00)
+to_rome = Flight.create(date: "2020-08-21", destination_id: rome.id, departing_time: "6:30 AM", arrival_time: "7:27 PM", price: 281.23)
+to_tokyo = Flight.create(date: "2021-08-21", destination_id: tokyo.id, departing_time: "7:05 AM", arrival_time: "2:26 AM", price: 327.29)
+to_sydney = Flight.create(date: "2020-08-21", destination_id: sydney.id, departing_time: "12:30 PM", arrival_time: "9:45 PM", price: 209.98)
 
 #Reservations
 
